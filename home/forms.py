474f9a42eb8c09cd6dc6d6712e4ff25843add93c
Advaid Sivadas <advaidsivadas@gmail.com>
2023-01-24 12:06:50 +0530

    initial commit

---
+++ home/forms.py
@@ -0,0 +1,25 @@
+
+from cProfile import label
+from django import forms
+from .models import Booking
+
+class DateInput(forms.DateInput):
+    input_type= 'date'
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+        widgets = {
+            'booking_date' : DateInput(),
+        }
+
+        labels ={
+             'p_name': "Patient Name:",
+             'p_phone':"Patient Phonenumber:",
+             'p_email':"Patient Email:",
+             'doc_name':"Doctor Name:",
+             'booking_date': "Booking Date:",
+
+        }
